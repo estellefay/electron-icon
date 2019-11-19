56ad1027a938ecd96488ebb0b76edb873aa5f299
Estellefay <estelle-fayolle@hotmail.fr>
2019-11-19 15:36:09 +0100

    algorithme

--- preload.js
+++ preload.js
@@ -5,8 +5,16 @@ var {dialog} = require('electron').remote
 // fsevent =>observe folder output
 // => generate thumb
 // const {exec} = require ('child_process');
-
+var canvas
+var ctx;
+var image;
 window.addEventListener('DOMContentLoaded', () => {
+
+
+  canvas = document.getElementById("canvas")
+  ctx = canvas.getContext('2d')
+ 
+  // Chargement de l'image
   document.getElementById('getImg').addEventListener('click', evt => {
     dialog.showOpenDialog({
       properties: ['openFile', 'multiSelections'],
@@ -14,48 +22,156 @@ window.addEventListener('DOMContentLoaded', () => {
         { name: 'All Files', extensions: ['svg', 'png'] }
       ],
     }).then(result => {
-      // console.log(result.canceled)
-      console.log(result.filePaths)
-
-      // Chercher le canva
-      var canvas = document.getElementById("canvas"), 
-      ctx = canvas.getContext('2d');
-
-      // Remove canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
       // Créer une nouvelle image
       var img = new Image();   // Crée un nouvel élément Image
-      // Ajouter l'image importer
-      img.src = result.filePaths
       // Charger l'image
       img.onload = () => {
-        // Définir le ration à 0
-        const imgRatio = img.width / img.height;
-        let startX = 0;
-        let startY = 0;
-        // SI portrait
-        if (imgRatio < 1) {
-          img.width = img.width * (ctx.canvas.height / img.height);
-          img.height = ctx.canvas.height;
-          startX = (ctx.canvas.width / 2) - (img.width / 2);
-        // Si paysage
-        } else if (imgRatio > 1) {
-          img.height = img.height * (ctx.canvas.width / img.width);
-          img.width = ctx.canvas.width;
-          startY = (ctx.canvas.height / 2) - (img.height / 2);
-        } else {
-          img.width = ctx.canvas.width;
-          img.height = ctx.canvas.height;
-        }
-        ctx.drawImage(img, startX, startY, img.width, img.height);
+        image = img ;
+        // relancer le reload
+        reload()
       }
-
-
+      // Ajouter l'image importer
+      img.src = result.filePaths
     }).catch(err => {
       console.log(err)
     })
   })
+  // j'écoute tous les inputs
+  // a chaque changement je reload
+  document.getElementById("primary_color").addEventListener('input', evt => {
+    reload();
+  }) 
+});
+
+
+// 
+  function reload() {
+    // Selectionner le canvas
+    // Nettoyer
+    ctx.clearRect(0, 0, 1024, 1024)
+    // Mettre un arrière pan
+    ctx.fillStyle = getColor()
+    // Définir la taille
+    ctx.fillRect(0, 0, 1024, 1024)
+
+    // Ajouter l'image
+    if (image) {
+    
+    console.log(image.src);
+    // Dessiner l'image
+    const imgRatio = image.width / image.height;
+    let startX = 0;
+    let startY = 0;
+    // Si portrait
+    if (imgRatio < 1) {
+      image.width = image.width * (ctx.canvas.height / image.height);
+      image.height = ctx.canvas.height;
+      startX = (ctx.canvas.width / 2) - (image.width / 2);
+    // Si paysage
+    } else if (imgRatio > 1) {
+      image.height = image.height * (ctx.canvas.width / image.width);
+      image.width = ctx.canvas.width;
+      startY = (ctx.canvas.height / 2) - (image.height / 2);
+    } else {
+      image.width = ctx.canvas.width;
+      image.height = ctx.canvas.height;
+    }
+    ctx.drawImage(image, startX, startY, image.width, image.height);
+  }
+    // AJouter les bordures
+}
+  
+/**
+ * Obtenir la couleur de l'arrière plan
+ */
+function getColor() {
+  var colorDiv =  document.getElementById("primary_color");
+  return colorDiv.value;
+}
+
+/**
+ * Obtenir les bordures
+ */
+function getBorder() {
+// TODO
+}
+
+
+/**
+ * Obtenir l'image
+ * @param
+ *  ctx 
+ */
+
+  
+
+
+
+  
+// faire des param 
+// param [
+//   test () => document.getElementById("tot");
+
+// ]
+
+ // document.getElementById('getImg').addEventListener('click', evt => {
+// lancer la fonction dès que un bouton est changer 
+
+
+
+  //   dialog.showOpenDialog({
+  //     properties: ['openFile', 'multiSelections'],
+  //     filters: [
+  //       { name: 'All Files', extensions: ['svg', 'png'] }
+  //     ],
+  //   }).then(result => {
+  //     // console.log(result.canceled)
+  //     console.log(result.filePaths)
+
+  //     // Chercher le canva
+  //     var canvas = document.getElementById("canvas"), 
+  //     ctx = canvas.getContext('2d');
+
+  //     // Remove canvas
+  //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //     // créer canva
+  //     // ctx.fillStyle  = backgroung color
+  //     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+  //     // Créer une nouvelle image
+  //     var img = new Image();   // Crée un nouvel élément Image
+  //     // Ajouter l'image importer
+  //     img.src = result.filePaths
+  //     // Charger l'image
+  //     img.onload = () => {
+  //       // Définir le ration à 0
+  //       const imgRatio = img.width / img.height;
+  //       let startX = 0;
+  //       let startY = 0;
+  //       // SI portrait
+  //       if (imgRatio < 1) {
+  //         img.width = img.width * (ctx.canvas.height / img.height);
+  //         img.height = ctx.canvas.height;
+  //         startX = (ctx.canvas.width / 2) - (img.width / 2);
+  //       // Si paysage
+  //       } else if (imgRatio > 1) {
+  //         img.height = img.height * (ctx.canvas.width / img.width);
+  //         img.width = ctx.canvas.width;
+  //         startY = (ctx.canvas.height / 2) - (img.height / 2);
+  //       } else {
+  //         img.width = ctx.canvas.width;
+  //         img.height = ctx.canvas.height;
+  //       }
+  //       ctx.drawImage(img, startX, startY, img.width, img.height);
+  //     }
+
+
+  //   }).catch(err => {
+  //     console.log(err)
+  //   })
+  // })
 
   // const replaceText = (selector, text) => {
   //   const element = document.getElementById(selector)
@@ -67,6 +183,3 @@ window.addEventListener('DOMContentLoaded', () => {
   // }
 
   // save file
-})
-
-
--- renderer.js
+++ renderer.js
@@ -7,11 +7,33 @@
 
 var colorButton = document.getElementById("primary_color");
 var colorDiv = document.getElementById("color_val");
-var colorDiv = document.getElementById("canvas");
+// var canvas = document.getElementById("canvas");
+
+// var border = document.querySelectorAll('input[type=radio][name="border"]');
+// context = canvas.getContext("2d");
+
+
 
 
     colorButton.onchange = function() {
         colorDiv.innerHTML = colorButton.value;
         colorDiv.style.color = colorButton.value;
-        canvas.style.backgroundColor = colorButton.value;
+        //canvas.style.backgroundColor = colorButton.value;
     }
+
+//     function changeHandler(event) {
+//         console.log(this.value);
+//         if ( this.value === 'around' ) {
+//             canvas.style.borderRadius = "20%";
+//         } else if ( this.value === 'square' ) {
+//             canvas.style.borderRadius = "0%";
+//         }  else if ( this.value === 'circle' ) {
+//             canvas.style.borderRadius = "100%";
+//          } 
+//      }
+     
+//      Array.prototype.forEach.call(border, function(border) {
+//         border.addEventListener('change', changeHandler);
+//      });
+
+
