148a9c6a1eeffd443e82990c6a0ada1a699c43d1
Estellefay <estelle-fayolle@hotmail.fr>
2019-11-20 11:57:33 +0100

    ajout zoom

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+var {fsmonitor} = require('fsmonitor');
 
 var {dialog} = require('electron').remote
 // fsevent =>observe folder output
@@ -8,9 +9,14 @@ var {dialog} = require('electron').remote
 var canvas
 var ctx;
 var image;
+let startX;
+let startY;
+
+
+// chokidar ou fsmonitor
+
+// lorsque on ajoute un fichier dans le filePath on générer les miniature
 window.addEventListener('DOMContentLoaded', () => {
-
-
   canvas = document.getElementById("canvas")
   ctx = canvas.getContext('2d')
  
@@ -36,11 +42,33 @@ window.addEventListener('DOMContentLoaded', () => {
       console.log(err)
     })
   })
+
   // j'écoute tous les inputs
   // a chaque changement je reload
+
+  // changement background color
   document.getElementById("primary_color").addEventListener('input', evt => {
     reload();
   }) 
+
+  // zoom
+  document.getElementById("zoom").addEventListener('input', evt => {
+    reload();
+  }) 
+// vertical
+document.getElementById("vertical").addEventListener('input', evt => {
+  reload();
+}) 
+
+// horizontal
+document.getElementById("horizontal").addEventListener('input', evt => {
+  reload();
+}) 
+  // border
+
+  // save
+
+  
 });
 
 
@@ -60,24 +88,46 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log(image.src);
     // Dessiner l'image
     const imgRatio = image.width / image.height;
-    let startX = 0;
-    let startY = 0;
+    // startX = 0;
+    // startY = 0;
     // Si portrait
     if (imgRatio < 1) {
       image.width = image.width * (ctx.canvas.height / image.height);
       image.height = ctx.canvas.height;
-      startX = (ctx.canvas.width / 2) - (image.width / 2);
+      //startX = (ctx.canvas.width / 2) - (image.width / 2);
     // Si paysage
     } else if (imgRatio > 1) {
       image.height = image.height * (ctx.canvas.width / image.width);
       image.width = ctx.canvas.width;
-      startY = (ctx.canvas.height / 2) - (image.height / 2);
+      //startY = (ctx.canvas.height / 2) - (image.height / 2);
     } else {
       image.width = ctx.canvas.width;
       image.height = ctx.canvas.height;
     }
-    ctx.drawImage(image, startX, startY, image.width, image.height);
-  }
+
+    var zoom = document.getElementById("zoom");
+    var horizontal = document.getElementById("horizontal");
+    var vertical = document.getElementById("vertical");
+    vertical = vertical.value
+    horizontal = horizontal.value
+
+    var scale = zoom.value;
+    // Zoom
+    var zh = image.height * scale
+    var zw = image.width * scale
+
+    // get vertical 
+    var iY = (1024 - zh)/2
+    // get horizontal
+    var iX = (1024 - zw)/2
+
+    iY = iY + vertical
+    iX = iX + horizontal
+
+    ctx.drawImage(image, iX, iY, zw, zh);
+    
+  } 
+
     // AJouter les bordures
 }
   
@@ -85,7 +135,7 @@ window.addEventListener('DOMContentLoaded', () => {
  * Obtenir la couleur de l'arrière plan
  */
 function getColor() {
-  var colorDiv =  document.getElementById("primary_color");
+var colorDiv =  document.getElementById("primary_color");
   return colorDiv.value;
 }
 
@@ -95,6 +145,7 @@ function getColor() {
 function getBorder() {
 // TODO
 }
+
 
 
 /**
